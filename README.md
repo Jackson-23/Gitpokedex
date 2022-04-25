0c4c45222a05c4847507151fc8d8fb5ee4825c69
Jackson-23 <jackson_1998@hotmail.com>
2022-04-25 20:48:30 -0300

    novo cadastro, delete em detalhes e Readme att.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Gitpokedex
 
-Pokedéx
-Projeto Pokedéx para o curso da BlueedTech
-Linkedin
+## Projeto
+Projeto 2 Pokedéx para o curso da BlueedTech Módulo 02, ferramentas utilizadas HTML, CSS, JavaScript, Node, express e EJS.
+
+## Github
+https://github.com/Jackson-23/Gitpokedex
+
+## Linkedin
+https://www.linkedin.com/in/jackson-nascimento-48a4b3231/
 
--- index.js
+++ index.js
@@ -6,111 +6,117 @@ const path = require("path");
 //configurações
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 
 //Array lista de objetos contendo dados dos pokemons
+let pokesquantid = 7
 const pokedex = [
-    {
-        id: 1,
-        nome: "Scorbunny",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/813.png"
-    },
+  {
+    id: 1,
+    nome: "Scorbunny",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/813.png",
+  },
 
-    {
-        id: 2,
-        nome: "Grookey",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/810.png"
-    },
+  {
+    id: 2,
+    nome: "Grookey",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/810.png",
+  },
 
-    {
-        id: 3,
-        nome: "Sobble",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/816.png"
-    },
+  {
+    id: 3,
+    nome: "Sobble",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/816.png",
+  },
 
-    {
-        id: 4,
-        nome: "Corviknight",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/823.png"
-    },
+  {
+    id: 4,
+    nome: "Corviknight",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/823.png",
+  },
 
-    {
-        id: 5,
-        nome: "Vulpix Alola",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/037_f2.png"
-    },
+  {
+    id: 5,
+    nome: "Vulpix Alola",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem:
+      "https://assets.pokemon.com/assets/cms2/img/pokedex/full/037_f2.png",
+  },
 
-    {
-        id: 6,
-        nome: "Umbreon",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png"
-    },
-    
-    {
-        id: 7,
-        nome: "Sprigatito",
-        descricao: "infos",
-        tipo: "Elemento",
-        imagem: "/images/Sprigatito2.png"
-    }
+  {
+    id: 6,
+    nome: "Umbreon",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png",
+  },
 
-
-
-    
-]
-
-
-
+  {
+    id: 7,
+    nome: "Sprigatito",
+    descricao: "infos",
+    tipo: "Elemento",
+    imagem: "/images/Sprigatito2.png",
+  },
+];
 
 //ROTAS
 app.get("/", (req, res) => {
-    res.render("index", {pokedex});
-    console.log(pokedex);
+  res.render("index", { pokedex });
 });
 
 app.post("/add", (req, res) => {
-    let pokemon = req.body;
-    console.log(pokemon);
-    pokemon.id = pokedex.length+1;
-    pokedex.push(pokemon);
-    console.log(pokemon.id);
-    res.redirect("/");
+  let pokemon = req.body;
+  console.log(pokemon);
+  pokemon.id = pokesquantid + 1;
+  pokesquantid++;
+  pokedex.push(pokemon);
+  console.log(pokemon.id);
+  res.redirect("/");
 });
 
-app.post("/update", (req, res)=>{
-    let pokemon = req.body;
-    console.log(pokemon);
-    //console.log("update"+pokemon.id);
-    pokedex[pokemon.id-1] = pokemon;
-    //const pokemon = pokedex.find(pokemon => pokemon.id === id);
-    res.render("detalhes", {pokemon});
+app.post("/update", (req, res) => {
+  let pokemon = req.body;
+  pokedex[pokemon.id - 1] = pokemon;
+  res.redirect(`detalhes/${req.body.id}`);
 });
 
 app.get("/detalhes/:id", (req, res) => {
-    const id = +req.params.id;
-    console.log(+req.params.id);
-    let pokemon = pokedex.find(pokemon => pokemon.id == id);
-    console.log(pokemon);
-    res.render("detalhes", {pokemon});
-})
-/*'https://assets.pokemon.com/assets/cms2/img/pokedex/full/823.png'*/
-
-app.post("/home", (req, res) => {
-    res.send("");
+  const id = +req.params.id;
+  console.log(+req.params.id);
+  let pokemon = pokedex.find((pokemon) => pokemon.id == id);
+  console.log(pokemon);
+  res.render("detalhes", { pokemon });
 });
 
-app.listen(3000, ()=>
-    console.log("Servidor rodando em http://localhost:3000")
+app.get("/cadastrar", (req, res) => {
+  res.render("cadastrar");
+});
+
+app.get("/deletar/:id", (req, res) => {
+  console.log(+req.params.idq);
+
+  const id = +req.params.id;
+  console.log(id + " delete");
+  const indice = pokedex.findIndex((pokemon) => pokemon.id == id);
+  pokedex.splice(indice, 1);
+  res.redirect("/");
+});
+
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(3000, () =>
+  console.log("Servidor rodando em http://localhost:3000")
 );
